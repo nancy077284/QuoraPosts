8155a3cac4e66e7ea6453325e4ab556ce55fd1de
nancy077284 <nancy077284@gmail.com>
2026-09-25 02:11:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+/*let n=5;
+for(let i=0;i<n;i++){
+console.log("hello, ", i);
+}
+
+console.log("bye...");
+
+...................................................
+
+
+const math=require("./math");
+console.log(math.sum(5, 3));
+.........................................
+
+*/
+const info = require("./Fruits");
+
+console.log(info);
+console.log(info[0].name);
